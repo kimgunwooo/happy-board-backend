d2f2fb3021ae92f7ea3d5848c7af677b0db63cea
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-06-02 21:00:59 +0900

    init : project

---
+++ src/main/java/likelion/ideateam3/happy_board/HappyBoardApplication.java
@@ -0,0 +1,13 @@
+package likelion.ideateam3.happy_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion/ideateam3/happy_board/HappyBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.ideateam3.happy_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
